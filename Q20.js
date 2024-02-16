ece22e452cfb2520c8ffaf9eb8f273709d959764
Sidra Gondal <150411149+geekycoder2@users.noreply.github.com>
2024-02-17 01:34:17 +0500

    Add files via upload

---
+++ Q20.js
@@ -0,0 +1,13 @@
+"use strict";
+// 20.Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+// List of Countries
+const countries = ["Pakistan", "Dubai", "Qatar", "Kuwait", "Japan", "London"];
+console.log("List of Countries:");
+for (let List of countries) {
+    console.log(List);
+}
+// List of Languages
+const languages = ["Urdu", "English", "Arabic", "French", "Chines", "Punjabi"];
+for (let List of languages) {
+    console.log(List);
+}
---
+++ Q20.ts
@@ -0,0 +1,16 @@
+// 20.Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+// List of Countries
+const countries: string []= ["Pakistan","Dubai","Qatar","Kuwait","Japan","London"]
+console.log("List of Countries:")
+for(let List of countries) 
+{
+console.log(List)
+}
+
+// List of Languages
+const languages: string []= ["Urdu","English","Arabic","French","Chines","Punjabi"]
+for (let List of languages)
+{
+    console.log(List)
+}
